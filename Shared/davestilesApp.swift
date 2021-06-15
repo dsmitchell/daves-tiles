e157577b4044bff97877a50e93a2328cbee6ef84
DS Mitchell <ds.mitchell@theappstudiollc.com>
2021-06-14 20:19:25 -0400

    Initial Commit

---
+++ Shared/davestilesApp.swift
@@ -0,0 +1,20 @@
+//
+//  davestilesApp.swift
+//  Shared
+//
+//  Created by The App Studio LLC on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct davestilesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
